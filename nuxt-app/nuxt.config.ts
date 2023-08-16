da23e3eb848a9a5e6b3aaae3d619e499aacd884d
sekiguchi <sekiguchi@framelunch.jp>
2023-08-16 09:11:23 +0900

    デプロイ先変更

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   srcDir: 'src/',
+  app: {
+    head: {
+      title: '旅行予定表',
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0" },
+      ]
+    }
+  }
 })
