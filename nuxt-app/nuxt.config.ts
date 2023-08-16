d42de400d073731c64f2c5c6ed4f00fd533eae98
sekiguchi <sekiguchi@framelunch.jp>
2023-08-16 09:16:06 +0900

    フォント

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -10,5 +10,8 @@ export default defineNuxtConfig({
         { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0" },
       ]
     }
-  }
+  },
+  css: [
+    '@/assets/global.scss',
+  ],
 })
